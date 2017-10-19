57a70bc0f6a02710e2dfece10b80d7a85f8f61eb
guedouarimohamed <mohamadovski999@gmail.com>
2017-10-20 00:15:32 +0200

    versio0.1

--- tp_IGL/VectorHelper.cs
+++ tp_IGL/VectorHelper.cs
@@ -8,5 +8,25 @@ namespace tp_IGL
 {
     class VectorHelper
     {
+        //c'est la table principale ou on va executer les methodes(tri,somme,invers...)
+        /// <summary>
+        /// c'est le Vector.
+        /// </summary>
+        private int[] Vector;
+
+        /// <summary>
+        /// la valeur de n indique le nombre des éléments du Vector.
+        /// "F:N.X.n".
+        /// </summary>
+        private int n;
+        /// <summary>
+        /// cette variable booleen indique si le vector est initialiser ou non.
+        /// "F:N.X.is_initialiser".
+        /// </summary>
+        private bool is_initialiser = false;
+        /// <summary>
+        /// c'est le constructeur (sans parametre).
+        /// "M:N.X.#ctor".
+        /// </summary>
     }
 }
